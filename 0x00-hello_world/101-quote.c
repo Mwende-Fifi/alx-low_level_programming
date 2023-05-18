b431da0d4d1d5277841f4336126ee59e61c5e738
Mwende-Fifi <mwendekiindu@gmail.com>
2023-05-18 13:37:19 -0700

    corrected 101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,10 @@
-#include <stdio.h>
 #include <unistd.h>
-/**
- * main - Prints out the last part of a quote in the standard error.
- * Return: 1 if success.
- */
+
 int main(void)
 {
-	write(2, "and that piece of art is useful - Dora Korpar, 2015-10-19\n", 58);
-	return (1);
+    const char message[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19
+";
+    const int length = sizeof(message) - 1;
+    const ssize_t written = write(2, message, length);
+    return (written == length) ? 1 : 0;
 }
