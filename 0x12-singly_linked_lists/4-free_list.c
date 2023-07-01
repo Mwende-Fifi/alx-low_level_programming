26d03b873d71940bb026c7538af484db04285515
Mwende-Fifi <mwendekiindu@gmail.com>
2023-07-01 14:02:31 +0300

    4. Free list

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -2,12 +2,11 @@
 #include "lists.h"
 
 /**
- * free_list - Frees a linked list
- * @head: The pointer to the first node of linked list
- *
- * Return: Nothing
- */
-
+  * free_list - Frees a linked list.
+  * @head: The pointer to the first node of linked list
+  *
+  * Return: Nothing
+  */
 void free_list(list_t *head)
 {
 	list_t *temp;
